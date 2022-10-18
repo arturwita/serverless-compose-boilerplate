5e8fed82ff4fd8b0a645648b1f06f28858b6d1c4
arturwita <witart1998@gmail.com>
2022-10-18 22:23:26 +0200

    feat: apply target structure

---
+++ services/migration/webpack.config.js
@@ -0,0 +1,4 @@
+const { createWebpackConfig } = require("../create-webpack-config");
+// import { createWebpackConfig } from "../create-webpack-config";
+
+module.exports = createWebpackConfig(__dirname);
